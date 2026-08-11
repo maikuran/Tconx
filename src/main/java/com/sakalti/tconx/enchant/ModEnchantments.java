03893c594e935c28a7834e803a70780c16aa5aa6
maikuran <abyssalx@outlook.jp>
2026-08-11 11:10:24 +0900

    Add Rarity import to ModEnchantments.java

--- src/main/java/com/sakalti/tconx/enchant/ModEnchantments.java
+++ src/main/java/com/sakalti/tconx/enchant/ModEnchantments.java
@@ -6,6 +6,7 @@ import com.sakalti.tconx.enchant.HemorrhageEnchantment;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Rarity;
 import com.sakalti.tconx.items.NuzwatItem;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
