cd05a66af50c3242e5a74e9cb8a930f916f17ba5
maikuran <abyssalx@outlook.jp>
2026-08-11 15:43:18 +0900

    Register ModMetals blocks and items in ModMain

--- src/main/java/com/sakalti/tconx/ModMain.java
+++ src/main/java/com/sakalti/tconx/ModMain.java
@@ -6,6 +6,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import com.sakalti.tconx.material.hachilite.ModMaterials;
 import com.sakalti.tconx.enchant.ModEnchantments;
 import com.sakalti.tconx.registry.ModMirzo;
+import com.sakalti.tconx.registry.ModMetals;
 
 @Mod("sakalti")
 public class ModMain {
@@ -13,6 +14,8 @@ public class ModMain {
     public ModMain() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ModEnchantments.ENCHANTMENTS.register(bus); 
+        ModMetals.BLOCKS.register(bus);      // ← 追加
+        ModMetals.ITEMS.register(bus);       // ← 追加
         ModMaterials.registerMaterials();
     }
 }
