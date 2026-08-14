6bdd07d5ee26f5e40f1d77932bc16f464ff16be2
maikuran <abyssalx@outlook.jp>
2026-08-15 08:15:25 +0900

    Update ModTiers.java

--- src/main/java/com/sakalti/ModTiers.java
+++ src/main/java/com/sakalti/ModTiers.java
@@ -10,9 +10,16 @@ import java.util.List;
 public class ModTiers {
 
     public static final Tier SUPER = TierSortingRegistry.registerTier(
-            new ForgeTier(5, 0, 0.0f, 0.0f, 0,
-                    ModTags.Blocks.NEEDS_SUPER_TOOL, () -> Ingredient.of(ModMetals.IGNIZ_INGOT.get())),
-            new net.minecraft.resources.ResourceLocation(ModMetals.MODID, "igniz"),
+            new ForgeTier(
+                    5,
+                    0,
+                    0.0f,
+                    0.0f,
+                    0,
+                    ModTags.Blocks.NEEDS_SUPER_TOOL,
+                    () -> Ingredient.of(ModMetals.IGNIZ_INGOT.get())
+            ),
+            new net.minecraft.resources.ResourceLocation(ModMetals.MODID, "super"),
             List.of(net.minecraft.world.item.Tiers.NETHERITE),
             List.of()
     );
