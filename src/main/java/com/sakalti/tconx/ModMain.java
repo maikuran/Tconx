69bb20b8687f55a8a81690dc5dfb946e4e7c30ac
maikuran <abyssalx@outlook.jp>
2026-08-14 12:01:22 +0900

    Update ModMain.java

--- src/main/java/com/sakalti/tconx/ModMain.java
+++ src/main/java/com/sakalti/tconx/ModMain.java
@@ -3,7 +3,6 @@ package com.sakalti.tconx;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import com.sakalti.tconx.material.hachilite.ModMaterials;
 import com.sakalti.tconx.enchant.ModEnchantments;
 import com.sakalti.tconx.ModMetals;
 
@@ -14,7 +13,6 @@ public class ModMain {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ModEnchantments.ENCHANTMENTS.register(bus); 
         ModMetals.BLOCKS.register(bus);      // ← 追加
-        ModMetals.ITEMS.register(bus);       // ← 追加
-        ModMaterials.registerMaterials();
+        ModMetals.ITEMS.register(bus);       // ←
     }
 }
