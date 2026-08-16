2e640a26fbf158f0610aa6f29d252883c655997a
maikuran <abyssalx@outlook.jp>
2026-08-16 18:17:24 +0900

    Add ModCreativeTabs for custom creative tabs

---
+++ src/main/java/com/sakalti/ModCreativeTabs.java
@@ -0,0 +1,103 @@
+package com.sakalti;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public final class ModCreativeTabs {
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ModMetals.MODID);
+
+    public static final RegistryObject<CreativeModeTab> SAKALTI_TAB =
+            CREATIVE_MODE_TABS.register("sakalti", () ->
+                    CreativeModeTab.builder()
+                            .title(Component.translatable("itemGroup.sakalti"))
+                            .icon(() -> new ItemStack(ModMetals.SEIREN_INGOT.get()))
+                            .displayItems((parameters, output) -> {
+
+                                // Hachilite
+                                output.accept(ModMetals.HACHILITE_ORE.get());
+                                output.accept(ModMetals.HACHILITE_BLOCK.get());
+                                output.accept(ModMetals.HACHILITE_RAW.get());
+                                output.accept(ModMetals.HACHILITE_INGOT.get());
+
+                                // Kanilite
+                                output.accept(ModMetals.KANILITE_ORE.get());
+                                output.accept(ModMetals.KANILITE_BLOCK.get());
+                                output.accept(ModMetals.KANILITE_RAW.get());
+                                output.accept(ModMetals.KANILITE_INGOT.get());
+
+                                // Igniz
+                                output.accept(ModMetals.IGNIZ_ORE.get());
+                                output.accept(ModMetals.IGNIZ_BLOCK.get());
+                                output.accept(ModMetals.IGNIZ_RAW.get());
+                                output.accept(ModMetals.IGNIZ_INGOT.get());
+
+                                // Chirite
+                                output.accept(ModMetals.CHIRITE_ORE.get());
+                                output.accept(ModMetals.CHIRITE_BLOCK.get());
+                                output.accept(ModMetals.CHIRITE_RAW.get());
+                                output.accept(ModMetals.CHIRITE_INGOT.get());
+
+                                // Momongaite
+                                output.accept(ModMetals.MOMONGAITE_ORE.get());
+                                output.accept(ModMetals.MOMONGAITE_BLOCK.get());
+                                output.accept(ModMetals.MOMONGAITE_RAW.get());
+                                output.accept(ModMetals.MOMONGAITE_INGOT.get());
+
+                                // Other metals
+                                output.accept(ModMetals.HERDYEEN_BLOCK.get());
+                                output.accept(ModMetals.HERDYEEN_INGOT.get());
+
+                                output.accept(ModMetals.HIROSWARI_BLOCK.get());
+                                output.accept(ModMetals.HIROSWARI_INGOT.get());
+
+                                output.accept(ModMetals.MARULITE_BLOCK.get());
+                                output.accept(ModMetals.MARULITE_INGOT.get());
+
+                                output.accept(ModMetals.PROXIA_BLOCK.get());
+                                output.accept(ModMetals.PROXIA_INGOT.get());
+
+                                output.accept(ModMetals.OUSWARI_BLOCK.get());
+                                output.accept(ModMetals.OUSWARI_INGOT.get());
+
+                                output.accept(ModMetals.AUROSTONE_BLOCK.get());
+                                output.accept(ModMetals.AUROSTONE_INGOT.get());
+
+                                output.accept(ModMetals.DEEPSTEEL_BLOCK.get());
+                                output.accept(ModMetals.DEEPSTEEL_INGOT.get());
+                                output.accept(ModMetals.DEEPCHUNK.get());
+
+                                output.accept(ModMetals.SEIREN_BLOCK.get());
+                                output.accept(ModMetals.SEIREN_INGOT.get());
+
+                                output.accept(ModMetals.CHIISTEEL_BLOCK.get());
+                                output.accept(ModMetals.CHIISTEEL_INGOT.get());
+
+                                output.accept(ModMetals.IOXIUM_BLOCK.get());
+                                output.accept(ModMetals.IOXIUM_INGOT.get());
+
+                                output.accept(ModMetals.DILONITE_BLOCK.get());
+                                output.accept(ModMetals.DILONITE_INGOT.get());
+
+                                output.accept(ModMetals.TIBERITE_BLOCK.get());
+                                output.accept(ModMetals.TIBERITE_INGOT.get());
+
+                                output.accept(ModMetals.OSTLUM_BLOCK.get());
+                                output.accept(ModMetals.OSTLUM_INGOT.get());
+                            })
+                            .build()
+            );
+
+    private ModCreativeTabs() {
+    }
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
