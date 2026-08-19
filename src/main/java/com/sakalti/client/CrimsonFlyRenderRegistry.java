afbc3b2b0cb1182b64f3b99e4080f8ead749fb3d
maikuran <abyssalx@outlook.jp>
2026-08-19 10:48:53 +0900

    Refactor CrimsonFlyRenderRegistry into ModClient

--- src/main/java/com/sakalti/client/CrimsonFlyRenderRegistry.java
+++ src/main/java/com/sakalti/client/CrimsonFlyRenderRegistry.java
@@ -1,8 +1,12 @@
 package com.sakalti.client;
 
+import com.sakalti.client.model.CrimsonFlyModel;
 import com.sakalti.client.renderer.CrimsonFlyRenderer;
 import com.sakalti.entity.CrimsonFlyEntity;
 
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,7 +17,23 @@ import net.minecraftforge.fml.common.Mod;
         bus = Mod.EventBusSubscriber.Bus.MOD,
         value = Dist.CLIENT
 )
-public class CrimsonFlyRenderRegistry {
+public class ModClient {
+
+    public static final ModelLayerLocation CRIMSON_FLY_LAYER =
+            new ModelLayerLocation(
+                    new ResourceLocation("sakalti", "crimson_fly"),
+                    "main"
+            );
+
+    @SubscribeEvent
+    public static void registerLayerDefinitions(
+            EntityRenderersEvent.RegisterLayerDefinitions event
+    ) {
+        event.registerLayerDefinition(
+                CRIMSON_FLY_LAYER,
+                CrimsonFlyModel::createBodyLayer
+        );
+    }
 
     @SubscribeEvent
     public static void registerRenderers(
