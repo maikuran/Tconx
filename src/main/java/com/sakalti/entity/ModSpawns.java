b643e498ad4969a2b989d6a455e8cc248d8d5830
maikuran <abyssalx@outlook.jp>
2026-08-19 11:27:05 +0900

    Update ModSpawns.java

--- src/main/java/com/sakalti/entity/ModSpawns.java
+++ src/main/java/com/sakalti/entity/ModSpawns.java
@@ -1,30 +1,63 @@
 package com.sakalti.entity;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biomes;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = "sakalti", bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber(
+        modid = "sakalti",
+        bus = Mod.EventBusSubscriber.Bus.FORGE
+)
 public class ModSpawns {
 
     @SubscribeEvent
-    public static void onBiomeLoading(BiomeLoadingEvent event) {
-
-        if (event.getName() != null
-                && event.getName().equals(Biomes.CRIMSON_FOREST.location())) {
-
-            event.getSpawns().addSpawn(
-                    MobCategory.MONSTER,
-                    new MobSpawnSettings.SpawnerData(
-                            CrimsonFlyEntity.CRIMSON_FLY.get(),
-                            20,
-                            2,
-                            4
-                    )
-            );
+    public static void onPotentialSpawns(
+            LevelEvent.PotentialSpawns event
+    ) {
+        // モンスターの自然スポーン候補だけを変更
+        if (event.getCategory() != MobCategory.MONSTER) {
+            return;
         }
+
+        if (!(event.getLevel() instanceof Level level)) {
+            return;
+        }
+
+        BlockPos pos = event.getPos();
+
+        // その地点のBiome
+        ResourceLocation biomeId =
+                level.getBiome(pos)
+                        .unwrapKey()
+                        .map(key -> key.location())
+                        .orElse(null);
+
+        if (biomeId == null) {
+            return;
+        }
+
+        // 真紅の森だけ
+        if (!biomeId.equals(
+                new ResourceLocation(
+                        "minecraft",
+                        "crimson_forest"
+                )
+        )) {
+            return;
+        }
+
+        event.addSpawnerData(
+                new MobSpawnSettings.SpawnerData(
+                        CrimsonFlyEntity.CRIMSON_FLY.get(),
+                        20,
+                        2,
+                        4
+                )
+        );
     }
 }
