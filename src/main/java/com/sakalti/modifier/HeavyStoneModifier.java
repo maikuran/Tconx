a2c35ad8feb96fcfa0c2fcbc2d89b876cb4377cd
maikuran <abyssalx@outlook.jp>
2026-08-22 15:13:52 +0900

    Update HeavyStoneModifier.java

--- src/main/java/com/sakalti/modifier/HeavyStoneModifier.java
+++ src/main/java/com/sakalti/modifier/HeavyStoneModifier.java
@@ -9,19 +9,19 @@ import slimeknights.tconstruct.library.tools.nbt.IModifierToolStack;
 
 public class HeavyStoneModifier extends Modifier {
 
-    public HeavyStoneModifier(int color) {
-        super(color);
+    public HeavyStoneModifier() {
+        super(); // 1.16.5 では引数なしのコンストラクタ
     }
 
     /**
      * 近接攻撃がヒットした後に発動 (1.16.5仕様)
      */
-    
-    public void afterMeleeHit(IModifierToolStack tool, int level, ToolAttackContext context, float damageDealt) {
+    @Override
+    public int afterMeleeHit(IModifierToolStack tool, int level, ToolAttackContext context, float damageDealt) {
         LivingEntity attacker = context.getAttacker();
 
         // サーバー側かつ攻撃者が存在する場合のみ処理
-        if (attacker != null && !attacker.level.isClientSide) {
+        if (attacker != null && !attacker.level.isClientSide()) {
 
             int durationTicks = 20 * 5; // 5秒持続
 
@@ -33,5 +33,7 @@ public class HeavyStoneModifier extends Modifier {
             attacker.addEffect(slowness);
             attacker.addEffect(resistance);
         }
+
+        return 0; // 1.16.5 では int を返します
     }
 }
