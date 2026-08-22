b8ed93cc40ba231dc6e3de2ac85c24ad86ffa8c9
maikuran <abyssalx@outlook.jp>
2026-08-22 17:55:12 +0900

    Update ModSpawns.java

--- src/main/java/com/sakalti/entity/ModSpawns.java
+++ src/main/java/com/sakalti/entity/ModSpawns.java
@@ -3,7 +3,6 @@ package com.sakalti.entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.MobSpawnInfo;
@@ -20,9 +19,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 )
 public class ModSpawns {
 
-    /**
-     * エンティティの属性登録
-     */
     @SubscribeEvent
     public static void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
         event.put(
@@ -31,25 +27,19 @@ public class ModSpawns {
         );
     }
 
-    /**
-     * スポーン配置（Placement）の登録
-     */
     @SubscribeEvent
     public static void onCommonSetup(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            EntitySpawnPlacementRegistry.register(
+            EntitySpawnPlacementRegistry.<CrimsonFlyEntity>register(
                     CrimsonFlyEntity.CRIMSON_FLY.get(),
                     EntitySpawnPlacementRegistry.PlacementType.ON_GROUND,
                     Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
-                    (EntityType entityType, net.minecraft.world.IServerWorld world, net.minecraft.entity.SpawnReason spawnType, net.minecraft.util.math.BlockPos pos, java.util.Random random) -> 
-                        MonsterEntity.checkMonsterSpawnRules((EntityType<? extends MonsterEntity>) entityType, world, spawnType, pos, random)
+                    (entityType, world, spawnType, pos, random) -> 
+                        MonsterEntity.checkMonsterSpawnRules((EntityType<? extends MonsterEntity>) (EntityType<?>) entityType, world, spawnType, pos, random)
             );
         });
     }
 
-    /**
-     * 真紅の森に Crimson Fly の自然スポーンを追加
-     */
     @Mod.EventBusSubscriber(
             modid = "sakalti",
             bus = Mod.EventBusSubscriber.Bus.FORGE
@@ -64,14 +54,13 @@ public class ModSpawns {
                 return;
             }
 
-            // minecraft:crimson_forest バイオームか判定
             if (biomeName.equals(new ResourceLocation("minecraft", "crimson_forest"))) {
                 event.getSpawns().getSpawner(EntityClassification.MONSTER).add(
                         new MobSpawnInfo.Spawners(
                                 CrimsonFlyEntity.CRIMSON_FLY.get(),
-                                20, // スポーン重み (Weight)
-                                2,  // 最小パック数
-                                4   // 最大パック数
+                                20,
+                                2,
+                                4
                         )
                 );
             }
