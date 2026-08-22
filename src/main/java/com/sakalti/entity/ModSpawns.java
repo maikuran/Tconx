40f443a74daeb570d8e8a808b28616e54b96f3a9
maikuran <abyssalx@outlook.jp>
2026-08-22 17:08:49 +0900

    Refactor Crimson Fly spawn registration and attributes

--- src/main/java/com/sakalti/entity/ModSpawns.java
+++ src/main/java/com/sakalti/entity/ModSpawns.java
@@ -6,7 +6,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
@@ -27,28 +27,27 @@ public class ModSpawns {
     public static void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
         event.put(
                 CrimsonFlyEntity.CRIMSON_FLY.get(),
-                CrimsonFlyEntity.createAttributes().build() // create() ではなく build() が標準的ですが、どちらでも可
+                CrimsonFlyEntity.createAttributes().build()
         );
     }
 
     /**
      * スポーン配置（Placement）の登録
-     * ※ これを登録しておかないと、BiomeLoadingEvent で追加しても出現しないかクラッシュすることがあります
      */
     @SubscribeEvent
     public static void onCommonSetup(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
             EntitySpawnPlacementRegistry.register(
                     CrimsonFlyEntity.CRIMSON_FLY.get(),
-                    EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, // 飛行型なら ON_GROUND や IN_AIR を用途に合わせて選択
+                    EntitySpawnPlacementRegistry.PlacementType.ON_GROUND,
                     Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
-                    MonsterEntity::checkMonsterSpawnRules // または独自に定義したスポーン条件メソッド
+                    MonsterEntity::checkMonsterSpawnRules
             );
         });
     }
 
     /**
-     * 真紅の森にCrimson Flyの自然スポーンを追加
+     * 真紅の森に Crimson Fly の自然スポーンを追加
      */
     @Mod.EventBusSubscriber(
             modid = "sakalti",
@@ -58,7 +57,6 @@ public class ModSpawns {
 
         @SubscribeEvent
         public static void onBiomeLoading(BiomeLoadingEvent event) {
-
             ResourceLocation biomeName = event.getName();
 
             if (biomeName == null) {
@@ -67,14 +65,14 @@ public class ModSpawns {
 
             // minecraft:crimson_forest バイオームか判定
             if (biomeName.equals(new ResourceLocation("minecraft", "crimson_forest"))) {
-                event.getSpawns()
-                        .getSpawner(EntityClassification.MONSTER)
-                        .add(new Biome.SpawnListEntry(
+                event.getSpawns().getSpawner(EntityClassification.MONSTER).add(
+                        new MobSpawnInfo.Spawners(
                                 CrimsonFlyEntity.CRIMSON_FLY.get(),
                                 20, // スポーン重み (Weight)
                                 2,  // 最小パック数
                                 4   // 最大パック数
-                        ));
+                        )
+                );
             }
         }
     }
