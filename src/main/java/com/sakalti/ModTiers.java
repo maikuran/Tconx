79d3599b40294de1ce1603516641b2ac23c0f1c8
maikuran <abyssalx@outlook.jp>
2026-08-22 14:54:58 +0900

    Refactor ModTiers to implement IItemTier
    Refactored ModTiers class to implement IItemTier interface and removed ForgeTier registration.

--- src/main/java/com/sakalti/ModTiers.java
+++ src/main/java/com/sakalti/ModTiers.java
@@ -1,26 +1,58 @@
 package com.sakalti;
 
-import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraftforge.common.ForgeTier;
-import net.minecraftforge.common.TierSortingRegistry;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
 
-import java.util.List;
+import java.util.function.Supplier;
 
-public class ModTiers {
+public enum ModTiers implements IItemTier {
+    
+    SUPER(5, 0, 0.0F, 0.0F, 0, () -> Ingredient.of(ModMetals.IGNIZ_INGOT.get()));
 
-    public static final Tier SUPER = TierSortingRegistry.registerTier(
-            new ForgeTier(
-                    5,
-                    0,
-                    0.0f,
-                    0.0f,
-                    0,
-                    ModTags.Blocks.NEEDS_SUPER_TOOL,
-                    () -> Ingredient.of(ModMetals.IGNIZ_INGOT.get())
-            ),
-            new net.minecraft.resources.ResourceLocation(ModMetals.MODID, "super"),
-            List.of(net.minecraft.world.item.Tiers.NETHERITE),
-            List.of()
-    );
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamageBonus;
+    private final int enchantability;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    ModTiers(int harvestLevel, int maxUses, float efficiency, float attackDamageBonus, int enchantability, Supplier<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamageBonus = attackDamageBonus;
+        this.enchantability = enchantability;
+        this.repairMaterial = new LazyValue<>(repairMaterial);
+    }
+
+    @Override
+    public int getUses() {
+        return this.maxUses;
+    }
+
+    @Override
+    public float getSpeed() {
+        return this.efficiency;
+    }
+
+    @Override
+    public float getAttackDamageBonus() {
+        return this.attackDamageBonus;
+    }
+
+    @Override
+    public int getLevel() {
+        return this.harvestLevel;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairMaterial.get();
+    }
 }
