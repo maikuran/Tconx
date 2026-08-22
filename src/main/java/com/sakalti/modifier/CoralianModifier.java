ec45371c17990c08b72e071fb86d14e59f1d465e
maikuran <abyssalx@outlook.jp>
2026-08-22 09:55:32 +0900

    Update CoralianModifier.java

--- src/main/java/com/sakalti/modifier/CoralianModifier.java
+++ src/main/java/com/sakalti/modifier/CoralianModifier.java
@@ -1,21 +1,32 @@
 package com.sakalti.modifier;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.world.World;
 import slimeknights.tconstruct.library.modifiers.Modifier;
-import slimeknights.tconstruct.library.tools.nbt.ToolStack;
+import slimeknights.tconstruct.library.tools.nbt.IModifierToolStack;
 
 public class CoralianModifier extends Modifier {
 
-    public void tick(ToolStack tool, int level, LivingEntity entity) {
-        if (!tool.isBroken()) {
-            MobEffectInstance waterBreathing = new MobEffectInstance(
-                MobEffects.WATER_BREATHING,
-                220,
-                0,
-                true,
-                false
+    public CoralianModifier(int color) {
+        super(color);
+    }
+
+    @Override
+    public void onInventoryTick(IModifierToolStack tool, int level, World world, LivingEntity entity, int itemSlot, boolean isSelected, boolean isCorrectSlot, ItemStack stack) {
+        // ツールが破損しておらず、手に持っている（またはスロットにある）場合
+        // 手に持っている時限定にする場合は `if (isSelected)` を条件に追加してください
+        if (!world.isClientSide && !tool.isBroken()) {
+            
+            // 水中息転換（水中採掘/呼吸）バフを付与 (効果時間: 220 ticks = 11秒)
+            EffectInstance waterBreathing = new EffectInstance(
+                    Effects.WATER_BREATHING,
+                    220,
+                    0,
+                    true,  // ambient (ビコン等の環境エフェクトか)
+                    false  // showParticles (パーティクルを表示するか)
             );
 
             entity.addEffect(waterBreathing);
