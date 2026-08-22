2bd6be3df9033543ea3313230789cc2dc0dcc77b
maikuran <abyssalx@outlook.jp>
2026-08-22 16:35:09 +0900

    Update CrimsonFlyEntity.java

--- src/main/java/com/sakalti/entity/CrimsonFlyEntity.java
+++ src/main/java/com/sakalti/entity/CrimsonFlyEntity.java
@@ -28,7 +28,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
-net.minecraftforge.fml.RegistryObject
+import net.minecraftforge.fml.RegistryObject
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.EnumSet;
