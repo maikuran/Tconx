f9089c455323f22e50bc114475fbbda082e1eabb
maikuran <abyssalx@outlook.jp>
2026-08-23 07:34:32 +0900

    Update ModMain.java

--- src/main/java/com/sakalti/ModMain.java
+++ src/main/java/com/sakalti/ModMain.java
@@ -4,8 +4,11 @@ import com.sakalti.modifier.TconxModifiers;
 import com.sakalti.entity.*;
 import com.sakalti.scaling.HealthCrystals;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ModMain.MODID)
@@ -14,17 +17,46 @@ public final class ModMain {
     public static final String MODID = "sakalti";
 
     public ModMain() {
+
         IEventBus modEventBus =
                 FMLJavaModLoadingContext.get().getModEventBus();
 
+        // ========================================================
+        // Registry
+        // ========================================================
+
         ModMetals.register(modEventBus);
+
         ModFluids.register(modEventBus);
 
         CrimsonFlyEntity.register(modEventBus);
+
         HealthCrystals.register(modEventBus);
 
         TconxModifiers.MODIFIERS.register(modEventBus);
 
         ModTiers.SUPER.getUses();
+
+        // ========================================================
+        // Common Setup
+        // ========================================================
+
+        modEventBus.addListener(this::setup);
+
+        // ========================================================
+        // Forge Events
+        // ========================================================
+
+        MinecraftForge.EVENT_BUS.register(ModOreGeneration.class);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+        event.enqueueWork(new Runnable() {
+            @Override
+            public void run() {
+                ModOreGeneration.registerConfiguredFeatures();
+            }
+        });
     }
 }
