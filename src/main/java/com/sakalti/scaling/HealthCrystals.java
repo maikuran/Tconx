d96bfca52415d168d2e68ac77c93f26f6b7f444f
maikuran <abyssalx@outlook.jp>
2026-08-22 16:48:26 +0900

    Fix import statement for RegistryObject

--- src/main/java/com/sakalti/scaling/HealthCrystals.java
+++ src/main/java/com/sakalti/scaling/HealthCrystals.java
@@ -16,7 +16,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.fml.RegistryObject
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.UUID;
