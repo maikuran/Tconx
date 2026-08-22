8962f6ccd289d0f8cfb283b464a05abb3db6675e
maikuran <abyssalx@outlook.jp>
2026-08-22 19:50:26 +0900

    Refactor ModMain class and update imports

--- src/main/java/com/sakalti/ModMain.java
+++ src/main/java/com/sakalti/ModMain.java
@@ -1,70 +1,46 @@
-package com.sakalti.entity;
+package com.sakalti;
 
-import net.minecraft.entity.EntityClassification;
-import net.minecraft.entity.EntitySpawnPlacementRegistry;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.monster.MonsterEntity;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.biome.MobSpawnInfo;
-import net.minecraft.world.gen.Heightmap;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import com.sakalti.modifier.TconxModifiers;
+import com.sakalti.entity.*;
+import com.sakalti.scaling.HealthCrystals;
+
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent; // インポート追加
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod.EventBusSubscriber(
-        modid = "sakalti",
-        bus = Mod.EventBusSubscriber.Bus.MOD
-)
-public class ModSpawns {
+@Mod(ModMain.MODID)
+public final class ModMain {
 
-    @SubscribeEvent
-    public static void onCommonSetup(FMLCommonSetupEvent event) {
+    public static final String MODID = "sakalti";
+
+    public ModMain() {
+        IEventBus modEventBus =
+                FMLJavaModLoadingContext.get().getModEventBus();
+
+        // アイテム・ブロック
+        ModMetals.register(modEventBus);
+
+        // 溶融液体
+        ModFluids.register(modEventBus);
+        CrimsonFlyEntity.register(modEventBus);
+        // ModCreativeTabs.register(modEventBus);
+        HealthCrystals.register(modEventBus);
+        
+        // TConX modifier
+        TconxModifiers.MODIFIERS.register(modEventBus);
+
+        // ModTiers の static 初期化を確実に実行
+        ModTiers.SUPER.getUses();
+
+        // setup イベントリスナーを登録
+        modEventBus.addListener(this::setup);
+    } // <-- コンストラクタはここで閉じます！
+
+    // setup メソッドはコンストラクタの外側に配置します
+    private void setup(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            EntitySpawnPlacementRegistry.<CrimsonFlyEntity>register(
-                    CrimsonFlyEntity.CRIMSON_FLY.get(),
-                    EntitySpawnPlacementRegistry.PlacementType.ON_GROUND,
-                    Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
-                    (entityType, world, spawnType, pos, random) ->
-                            MonsterEntity.checkMonsterSpawnRules(
-                                    (EntityType<? extends MonsterEntity>) (EntityType<?>) entityType,
-                                    world,
-                                    spawnType,
-                                    pos,
-                                    random
-                            )
-            );
+            ModOreGeneration.registerConfiguredFeatures();
         });
-    }
-
-    @Mod.EventBusSubscriber(
-            modid = "sakalti",
-            bus = Mod.EventBusSubscriber.Bus.FORGE
-    )
-    public static class BiomeSpawns {
-
-        @SubscribeEvent
-        public static void onBiomeLoading(BiomeLoadingEvent event) {
-            ResourceLocation biomeName = event.getName();
-
-            if (biomeName == null) {
-                return;
-            }
-
-            if (biomeName.equals(
-                    new ResourceLocation("minecraft", "crimson_forest"))) {
-
-                event.getSpawns()
-                        .getSpawner(EntityClassification.MONSTER)
-                        .add(
-                                new MobSpawnInfo.Spawners(
-                                        CrimsonFlyEntity.CRIMSON_FLY.get(),
-                                        20,
-                                        2,
-                                        4
-                                )
-                        );
-            }
-        }
     }
 }
